b6c5eab3d577fdec61bc8d435a27c4391d7162d2
hakimnazry24 <hakimnazry@gmail.com>
2025-03-11 10:43:53 +0800

    firfstA

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,16 @@ import '../global.css';
 
 import { Stack } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+// export const unstable_settings = {
+//   // Ensure that reloading on `/modal` keeps a back button present.
+//   initialRouteName: '(tabs)',
+// };
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      </Stack>
   );
 }
---
+++ app/login.tsx
@@ -0,0 +1,43 @@
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Link } from 'expo-router';
+import { useState } from 'react';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isShowPassword, setIsShowPassword] = useState(false);
+
+  return (
+    <View className="p-6 py-20">
+      <Text className=" mb-7 text-3xl font-semibold">Login</Text>
+      <Text className="text-lg">If you don't have an account, you can</Text>
+      <Text className="mb-16 text-lg font-semibold text-blue-700 underline">
+        <Link href="/register">Register now!</Link>
+      </Text>
+
+      <Text className="text-gray-500">Email</Text>
+      <TextInput
+        className="mb-6 border-b-2 p-4 focus:border-blue-700"
+        placeholder="Enter your email address"
+        value={email}
+        onChangeText={(newText) => setEmail(newText)}
+      />
+
+      <Text className="text-gray-500">Password</Text>
+      <TextInput
+        className="mb-2 border-b-2 p-4 focus:border-r-blue-700"
+        placeholder="Enter your password"
+        value={password}
+        secureTextEntry={!isShowPassword}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+      />
+      <Text className="text-gray-500" onPress={() => setIsShowPassword(!isShowPassword)}>{isShowPassword ? 'Hide' : 'Show'} password</Text>
+
+      <TouchableOpacity className="mb-10 mt-20">
+        <Text className="mx-auto w-full rounded-full bg-blue-700 py-4 text-center font-semibold text-white">
+          Login
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
